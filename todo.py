3c677cdea7a95b8ffe070472183d52a6a806f96d
mehmet yasti <mehmet.yasti@velux.com>
2022-11-27 21:41:09 +0100

    feat: health endpoint added

--- todo.py
+++ todo.py
@@ -5,7 +5,7 @@ import os
 PWD = os.path.abspath(os.path.dirname(__file__))
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__,template_folder='templates')
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(os.path.join(PWD, 'todo.db'))
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db = SQLAlchemy(app)
@@ -22,6 +22,7 @@ class Todo(db.Model):
 def index():
     todos = Todo.query.all()
     return render_template("index.html",todos = todos)
+    
 
 @app.route("/add",methods=["POST"])
 def addTodo():
@@ -44,6 +45,10 @@ def delete(id):
     db.session.delete(todo)
     db.session.commit()
     return redirect(url_for("index"))
+
+@app.route("/health",methods=["GET"])
+def health():
+    return "Healthy"
 
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0",port=5001)
